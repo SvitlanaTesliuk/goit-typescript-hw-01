db3b4b670f32f46f02262b84d5c1e0a604d84ee2
SvitlanaTesliuk <svitlanateslyuk277@gmail.com>
2024-12-22 13:35:27 +0200

    fix generics/3.ts

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+
 async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
-    return response.data as T;
+return response.data as T;
+
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,6 @@
-function merge<T, U>(objA: T, objB: U): T & U {
-    return Object.assign({}, objA, objB);
-  }
-  
-  console.log(merge);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
+}
+
+const mergedObject = merge({ name: 'Alice' }, { age: 30 });
+console.log(mergedObject); 
